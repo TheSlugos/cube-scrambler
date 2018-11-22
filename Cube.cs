93cae8f39b327e6d81fad98160caffd321b369fd
Stephen Powell <steve.powell36@gmail.com>
2018-11-22 23:11:31 +1030

    Completed face rotations

--- Cube.cs
+++ Cube.cs
@@ -246,36 +246,205 @@ namespace cube_scrambler
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.RIGHT].RotateFaceCW();
+
+      // rotating right column for up/front and down, left column for back
+      // store front face right column in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // FRONT face right column contains tiles from DOWN face right column
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // DOWN face contains tiles from BACK face, but left col and the rows are inverted
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // BACK face left column contains tiles from UP face right column, rows inverted
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // UP face right column contains tiles from FRONT face right column, stored in temp
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = temp[CubeHelper.ROWTOP];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = temp[CubeHelper.ROWBOT];
+
     }
 
     private void RotateRW()
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.RIGHT].RotateFaceCCW();
+
+      // moving right col, except for BACK which is moving left col
+
+      // store front face right column in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // FRONT face right column contains tiles from UP face right column
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.FRONT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // UP face contains tiles from BACK face, but left col and the rows are inverted
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // BACK face left column contains tiles from DOWN face right column, rows inverted
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.BACK].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // DOWN face right column contains tiles from FRONT face right column, stored in temp
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = temp[CubeHelper.ROWTOP];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = temp[CubeHelper.ROWBOT];
     }
 
     private void RotateF()
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.FRONT].RotateFaceCW();
+
+      // left - right col -> up - bottom row -> right - left column -> down - top row
+
+      // store left face right col in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // left face right column gets down face top row
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+
+      // down face top row gets right face left column
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // right face left column gets up face bottom row
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // up face bottom row gets left face right col - stored in temp
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = temp[CubeHelper.ROWTOP];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = temp[CubeHelper.ROWBOT];
     }
 
     private void RotateFW()
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.FRONT].RotateFaceCCW();
+
+      // left - right col -> down - top row -> right - left col -> up - bottom row
+
+      // store left face right col in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // left face right column gets up face bottom row
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // up face bottom row gets right face left column
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // right face left column gets down face top row
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // down face top row gets left face right col - stored in temp
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = temp[CubeHelper.ROWBOT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = temp[CubeHelper.ROWTOP];
     }
 
     private void RotateB()
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.BACK].RotateFaceCW();
+
+      // left - left col -> down - bottom row -> right - right col -> up - top row
+
+      // store left face left col in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // left face right column gets up face bottom row
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // up face top row gets right face right column
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // right face right column gets down face bottom row
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // down face bottom row gets left face left col - stored in temp
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = temp[CubeHelper.ROWBOT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = temp[CubeHelper.ROWTOP];
+
     }
 
     private void RotateBW()
     {
       // Apply rotation to face tiles
       _Faces[CubeHelper.BACK].RotateFaceCCW();
+
+      // left - left col -> up - top row -> right - right column -> down - bottom row
+
+      // store left face left col in temp
+      string[] temp = new string[CubeHelper.TOTAL_TILES];
+      temp[CubeHelper.ROWTOP] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+      temp[CubeHelper.ROWMID] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT];
+      temp[CubeHelper.ROWBOT] = _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+
+      // left face left column gets down face bottom row
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWMID, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID];
+      _Faces[CubeHelper.LEFT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+
+      // down face bottom row gets right face right column
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLLFT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLMID] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT];
+      _Faces[CubeHelper.DOWN].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+
+      // right face right column gets up face top row
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWBOT, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWMID, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID];
+      _Faces[CubeHelper.RIGHT].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT];
+
+      // up facetop row gets left face left col - stored in temp
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLRGT] = temp[CubeHelper.ROWTOP];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLMID] = temp[CubeHelper.ROWMID];
+      _Faces[CubeHelper.UP].Tiles[CubeHelper.ROWTOP, CubeHelper.COLLFT] = temp[CubeHelper.ROWBOT];
     }
 
     public void ResetCube()
